3841b5d9af337b5f1cb27f8100a33ad81a47bc4b
Derda Dominik <MRD3RDA@gmail.com>
2025-06-28 22:52:24 +0200

    A Projects részt alakitgatam

--- Javascript/projects.js
+++ Javascript/projects.js
@@ -1,3 +1,4 @@
+
 document.addEventListener("DOMContentLoaded", function () {
     fetch("/../../../Javascript/projects.json")
         .then(response => response.json())
@@ -8,15 +9,32 @@ document.addEventListener("DOMContentLoaded", function () {
             projects.forEach(project => {
                 const card = document.createElement("div");
                 card.className = "card custom-projects p-3";
-                card.innerHTML = `
-                    <div class="card-body text-center">
-                        <h3 class="card-title">${project.icon} ${project.name}</h3>
-                        <p class="card-text">${project.description}</p>
-                        <a href="${project.url}" class="btn btn-primary" target="_blank">Megtekintés</a>
-                    </div>
-                `;
+                // card.innerHTML = `
+                //     <div class="card-body text-center">
+                //         <h3 class="card-title">${project.icon} ${project.name}</h3>
+                //         <p class="card-text">${project.description}</p>
+                //          <img class="services__card" src="${project.screenshot}" alt="Ez egy kép">
+                //         <a href="${project.url}" class="button" target="_blank">Continue</a>
+                //     </div>
+                // `;
+          card.innerHTML = `
+    <div 
+        class="services__card" 
+        style="background-image: linear-gradient(to bottom, rgba(0,0,0,0) 0%, rgba(17,17,17,0.6) 100%), url('${project.screenshot}');"
+        onclick="window.open('${project.url}', '_blank')"
+    >
+        <h3 class="card-title">${project.icon} ${project.name}</h3>
+        <div class="card-overlay">
+            <a href="${project.url}" target="_blank">Continue</a>
+        </div>
+    </div>
+    <p>Description:</p>
+    <p>${project.description}</p>
+`;
+
                 container.appendChild(card);
             });
         })
+    
         .catch(error => console.error("Hiba a projektek betöltése közben:", error));
 });
