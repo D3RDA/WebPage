b3f2b4cfe26ecfeab702e76b6813d6eb45d00522
Derda Dominik <MRD3RDA@gmail.com>
2025-06-28 15:36:37 +0200

    A weboldalt feltöltöttem

---
+++ Önéletrajzos/app.js
@@ -0,0 +1,19 @@
+const menu = document.querySelector('#mobile-menu')
+const menuLinks = document.querySelector('.navbar__menu')
+menu.addEventListener('click', function() {
+    menu.classList.toggle('is-active');
+    menuLinks.classList.toggle('active');
+    
+})
+const observer = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+            console.log(entry)
+            if (entry.isIntersecting){
+                entry.target.classList.add('show');
+            
+            }
+    });
+});
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
+
